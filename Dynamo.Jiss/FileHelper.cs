0aa9edf5691b315eb3bec06533f9f60f02deef52
Martin From <from@pc.dk>
2012-04-11 06:43:11 +0200

    Update to helpers and

--- Dynamo.Jiss/FileHelper.cs
+++ Dynamo.Jiss/FileHelper.cs
@@ -1,36 +1,85 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
 
 // IO Helper ?
+
+// For small files reading full content using StringBuilder into memory is okay.
+// Change if needed to be used with big files.
 
 namespace Dynamo.Jiss
 {
 	public static class FileHelper
 	{
-		public static void WriteFile(string filename, params string[] sources)
+		public static string GetContentCombined(params string[] files)
 		{
-			WriteFile(filename, sources);
+			if (files == null)
+				throw new ArgumentNullException("files");
+
+			var builder = new StringBuilder();
+			foreach (var file in files)
+			{
+				var content = File.ReadAllText(file);
+				builder.Append(content);
+			}
+
+			return builder.ToString();
 		}
 
-		public static void WriteFile(string filename, IEnumerable<string> sourceFiles, FileMode fileMode = FileMode.Create, FileAccess fileAccess = FileAccess.Write, int bufferSize = 1024)
+		public static bool TryWriteFile(string fullname, out string content, params string[] files)
 		{
-			if (filename == null)
-				throw new ArgumentNullException("filename");
-			if (sourceFiles == null)
-				throw new ArgumentNullException("sourceFiles");
+			if (fullname == null)
+				throw new ArgumentNullException("fullname");
+			if (files == null)
+				throw new ArgumentNullException("files");
 
-			using (var stream = new FileStream(filename, fileMode, fileAccess))
+			try
 			{
-				foreach (var source in sourceFiles)
-				{
-					// Copy all to the stream
-					using (var sourceStream = new FileStream(source, FileMode.Open, FileAccess.Read))
-					{
-						sourceStream.CopyTo(stream, bufferSize);
-					}
-				}
+				content = GetContentCombined(files);
+				File.WriteAllText(fullname, content);
+				return true;
 			}
+			catch (Exception)
+			{
+			}
+
+			content = null;
+			return false;
+
+
+
+			//using (var stream = new FileStream(fullname, fileMode, fileAccess))
+			//{
+			//    foreach (var source in sourceFiles)
+			//    {
+			//        // Copy all to the stream
+			//        using (var sourceStream = new FileStream(source, FileMode.Open, FileAccess.Read))
+			//        {
+			//            sourceStream.CopyTo(stream, bufferSize);
+			//        }
+			//    }
+			//}
+
+
+
+			// Read line by line ?
+
+			//foreach (var file in files)
+			//{
+			//    using (TextWriter tw = new StreamWriter(Path.GetFullPath(output), true))
+			//    {
+			//        using (StreamReader tr = new StreamReader(file))
+			//        {
+			//            while (!tr.EndOfStream)
+			//            {
+			//                tw.WriteLine(tr.ReadLine());
+			//            }
+			//            tr.Close();
+			//        }
+			//        tw.Close();
+			//    }
+			//}
 		}
 	}
 }
